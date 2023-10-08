b471e03f9de8332798beb6dc27d648204dad4a4b
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-08 14:36:45 +0600

    new product variant add route added

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAdmin } = require("../middlewares/adminMiddleware");
+const {
+  createProduct,
+  updateProduct,
+  getProducts,
+  updateProductVariant,
+  deleteProductVariant,
+  addNewProductVraiant,
+} = require("../services/products/productController");
 
-router.get("/");
+router.get("/", getProducts);
 
+router.post("/create", ensureAdmin, createProduct);
+
+router.put("/:id", ensureAdmin, updateProduct);
+
+router.put("/:productId/variant/:variantId", ensureAdmin, updateProductVariant);
+router.delete(
+  "/:productId/variant/:variantId",
+  ensureAdmin,
+  deleteProductVariant
+);
+
+router.put("/:productId/variant", ensureAdmin, addNewProductVraiant);
 module.exports = {
   path: "/products",
   router,
