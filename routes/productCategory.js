13dcc778ca61ab74c288c194659adfaa022e644d
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-09 18:00:21 +0600

    file upload func added

--- routes/productCategory.js
+++ routes/productCategory.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const s3 = require("../utils/s3-config/fileUploadConfig");
+
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
 const { ensureAdmin } = require("../middlewares/adminMiddleware");
 const {
   createProductCategory,
@@ -7,12 +12,24 @@ const {
   updateProductCategory,
 } = require("../services/product-category/productCategoryController");
 
-router.post("/create", ensureAdmin, createProductCategory);
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    bucket: "bazar-ghat",
+    key: function (req, file, cb) {
+      //   console.log("multer req......>", file);
+      cb(null, Date.now().toString() + file.originalname);
+    },
+  }),
+});
 
-router.get("/", getProductCategories);
-router.put("/:id", ensureAdmin, updateProductCategory);
+router.post(
+  "/categories/create",
+  upload.single("category-image"),
+  createProductCategory
+);
+router.get("/categories", getProductCategories);
+router.put("/categories/:id", ensureAdmin, updateProductCategory);
 
-module.exports = {
-  path: "/categories", // Define the base path for these routes
-  router,
-};
+module.exports = router;
