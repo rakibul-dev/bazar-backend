911686afdd4cb45d13e56d492ae7a66829ea1261
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-09 18:13:11 +0600

    product model updated

--- services/products/productModel.js
+++ services/products/productModel.js
@@ -1,41 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-    required: true,
-  },
-  variants: [
-    {
-      size: {
-        type: String,
-        // required: true,
-      },
-      color: {
-        type: String,
-        // required: true,
-      },
-      price: {
-        type: Number,
-        // required: true,
-      },
-      quantity: {
-        type: Number,
-        // required: true,
-      },
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    image: [{ type: String }],
+    variants: [
+      {
+        size: {
+          type: String,
+          // required: true,
+        },
+        color: {
+          type: String,
+          // required: true,
+        },
+        price: {
+          type: Number,
+          // required: true,
+        },
+        quantity: {
+          type: Number,
+          // required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
