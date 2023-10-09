dc42a95fca37b4c8712c0ec8953d927939c94c5d
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-09 18:14:00 +0600

    product category model updated

--- services/product-category/productCategoryModel.js
+++ services/product-category/productCategoryModel.js
@@ -1,22 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productCategorySchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const productCategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    parentCategory: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductCategory",
+    },
   },
-  status: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  parentCategory: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ProductCategory",
-  },
-});
+  { timestamps: true }
+);
 
 const productCategory = mongoose.model(
   "ProductCategory",
