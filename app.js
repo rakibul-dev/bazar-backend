343e5489325d77ea7754afee28e388c534cbc4a7
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-11 18:43:38 +0600

    session system updated

--- app.js
+++ app.js
@@ -14,11 +14,10 @@ const fs = require("fs");
 
 const passport = require("passport");
 require("dotenv").config();
-// Your configuration file
 
 const app = express();
 app.use(morgan("tiny"));
-// app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // Middleware
@@ -26,13 +25,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://example.com"],
+    origin: ["http://localhost:5173"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     // preflightContinue: true,
     // optionsSuccessStatus: 204,
   })
 );
+
+// var allowCrossDomain = function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// };
+// app.use(allowCrossDomain);
 
 // Connect to MongoDB
 mongoose
@@ -50,42 +57,6 @@ mongoose
     console.error("MongoDB connection error: ", err);
   });
 
-// required for passport session
-// app.use(
-//   session({
-//     secret: "secrettexthere",
-//     saveUninitialized: true,
-//     resave: true,
-//     // using store session on MongoDB using express-session + connect
-//     store: new MongoStore({
-//       url: `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@my-cluster.wlkgtiv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-//       collection: "sessions",
-//     }),
-//   })
-// );
-
-// app.use(
-//   require("express-session")({
-//     secret: process.env.sessionSecret,
-//     cookie: {
-//       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-//     },
-//     store: store,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: { secure: false }, // Remember to set this
-//   })
-// );
-
-// app.use(
-//   session({
-//     secret: "secret",
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
-
-// const rediscli = redis.createClient();
 let redisClient = redis.createClient({
   password: "8xUqC1qcsOk7vAK1f8snb6OiJJPurPa1",
   socket: {
@@ -109,9 +80,11 @@ let redisStore = new RedisStore({
 app.use(
   session({
     store: redisStore,
+    // cookie: { maxAge: 5000 },
     maxAge: 365 * 24 * 60 * 60 * 1000,
     resave: false, // required: force lightweight session keep alive (touch)
-    saveUninitialized: false, // recommended: only save session when data exists
+    saveUninitialized: true, // recommended: only save session when data exists
+
     secret: "keyboard_cat",
   })
 );
@@ -132,10 +105,6 @@ function loadRoutes(app) {
     // console.log(routeModule);
 
     app.use(routeModule);
-    // if (routeModule.path && routeModule.router) {
-    //   //   app.use(routeModule.router);
-    //   console.log(router);
-    // }
   });
 }
 
