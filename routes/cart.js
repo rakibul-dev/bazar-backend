df8a1601263f7c69311a91891c4760080d56d3db
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-17 23:26:24 +0600

    cart routes updated

--- routes/cart.js
+++ routes/cart.js
@@ -4,9 +4,13 @@ const { ensureAdmin } = require("../middlewares/adminMiddleware");
 const {
   addToCart,
   removeCartItem,
+  updateCartItemQuantity,
+  getCartItems,
 } = require("../services/cart/cartController");
 
 router.post("/cart/:userId", addToCart);
-router.put("/cart/:userId", removeCartItem);
+router.put("/cart/:id", removeCartItem);
+router.put("/cart/product/quantity/:id", updateCartItemQuantity);
+router.get("/cart/:id", getCartItems);
 
 module.exports = router;
