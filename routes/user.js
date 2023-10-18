a59b09057ddada3f6bfd6ed248735b0b0259202d
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-18 17:32:26 +0600

    current user info route added

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,13 @@ const {
   getUsers,
   getUserbyId,
   getCustomers,
+  getUser,
 } = require("../services/user/UserController");
 const { ensureAdmin } = require("../middlewares/adminMiddleware");
 
 router.get("/users", getUsers);
 router.get("/users/customer", ensureAdmin, getCustomers);
 router.get("/users/:id", getUserbyId);
+router.get("/user", getUser);
 
 module.exports = router;
